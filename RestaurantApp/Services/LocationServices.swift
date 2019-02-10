b35a4c0817d83a36d4a53d47e9837faf5212bee7
kunaltyagi26 <kunaltyagi32@gmail.com>
2019-02-10 17:16:50 +0530

    Added location service.

---
+++ RestaurantApp/Services/LocationServices.swift
@@ -0,0 +1,52 @@
+//
+//  LocationServices.swift
+//  RestaurantApp
+//
+//  Created by Kunal Tyagi on 10/02/19.
+//  Copyright © 2019 Kunal Tyagi. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+final class LocationService: NSObject {
+    private let locationManager: CLLocationManager
+    
+    init(manager: CLLocationManager) {
+        self.locationManager = manager
+        super.init()
+        manager.delegate = self
+    }
+    
+    var newLocation: ((Result<CLLocation>) -> Void)?
+    var didChangeStatus: ((Bool) -> Void)?
+    
+    var status: CLAuthorizationStatus {
+        return CLLocationManager.authorizationStatus()
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        newLocation?(.success(location))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        newLocation?(.failure(error))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined, .restricted, .denied:
+            didChangeStatus?(false)
+        default:
+            didChangeStatus?(true)
+        }
+    }
+}
